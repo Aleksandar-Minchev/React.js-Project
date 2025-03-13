852d0760b1e74a578ce3416efeb7c6e600fa89a1
Aleksandar Minchev <aminchev1988@gmail.com>
2025-03-13 16:26:51 +0200

    Add buttons array

---
+++ src/buttons.jsx
@@ -0,0 +1,77 @@
+export const buttons = [
+    {
+      "id": 1,
+      "title": "C",
+      "class": "clear"
+    },
+    {
+      "id": 2,
+      "title": "/"
+    },
+    {
+      "id": 3,
+      "title": "*"
+    },
+    {
+      "id": 4,
+      "title": "7"
+    },
+    {
+      "id": 5,
+      "title": "8"
+    },
+    {
+      "id": 6,
+      "title": "9"
+    },
+    {
+      "id": 7,
+      "title": "-"
+    },
+    {
+      "id": 8,
+      "title": "4"
+    },
+    {
+      "id": 9,
+      "title": "5"
+    },
+    {
+      "id": 10,
+      "title": "6"
+    },
+    {
+      "id": 11,
+      "title": "+",
+      "class": "plus"
+    },
+    {
+      "id": 12,
+      "title": "1"
+    },
+    {
+      "id": 13,
+      "title": "2"
+    },
+    {
+      "id": 14,
+      "title": "3"
+    },
+    {
+      "id": 15,
+      "title": "0"
+    },
+    {
+      "id": 16,
+      "title": "00"
+    },
+    {
+      "id": 17,
+      "title": "."
+    },
+    {
+      "id": 18,
+      "title": "=",
+      "class": "equal"
+    },
+  ]
